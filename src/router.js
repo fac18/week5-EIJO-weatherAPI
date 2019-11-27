b2e8b70837853415a2f163e09a6df457943a8418
renatajarmova <renatajarmova@gmail.com>
2019-11-27 15:16:29 +0000

    Add general server/handler/router content to be adapted later

--- src/router.js
+++ src/router.js
@@ -1,1 +1,19 @@
-let x = x;
+const handlers = require('./handlers.js')
+const router = (request, response) => {
+    const endpoint = request.url;
+    
+    if (endpoint === '/') {
+        handlers.handleHome(request, response);
+
+    } else if (endpoint.includes('public')) {
+        handlers.handlePublic(request, response, endpoint); // PASS THE URL 
+    
+    // } else if (endpoint.includes("search")) {
+    //     handlers.handleData(request, response, endpoint);
+           
+    } else {
+        response.writeHead(404);
+        response.end('404 not found');
+    }
+}
+module.exports = router;
