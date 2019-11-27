46ecb5b5e3df227ffbaeabb70da7c764f7e9f91e
Reda <redahaq@gmail.com>
2019-11-27 16:59:40 +0000

    rename api req function
    Co-authored-by: Maria Lani <m.olanipekun@hotmail.com>

--- src/api.js
+++ src/api.js
@@ -9,13 +9,13 @@ const weatherKey = config.WEATHER_KEY; // fetch key from config file
 
 console.log(weatherKey);
 
-const url = `api.openweathermap.org/data/2.5/weather? ${queryString} ${weatherKey}`;
-const myRequest = (url, cb) => {
-  const protocol = url.includes("https") ? https : http;
+const url = `http://api.openweathermap.org/data/2.5/weather? ${queryString} ${weatherKey}`;
+const getWeather = (url, cb) => {
+const protocol = url.includes("https") ? https : http;
   protocol.get(url, response => {
       let data = "";
       response.on("data", chunk => {
-        data += chunk;
+      data += chunk;
       });
       response.on("end", () => {
         const body = JSON.parse();
@@ -26,23 +26,8 @@ const myRequest = (url, cb) => {
     })
     .on("error", err => cb(err));
 };
-// uncomment below for bonus https solution
-// 
-//     .get(url, response => {
-//       let data = "";
-//       response.on("data", chunk => {
-//         data += chunk;
-//       });
-//       response.on("end", () => {
-//         const body = JSON.parse(data);
-//         const statusCode = response.statusCode;
-//         cb(null, { statusCode, body });
-//       });
-//     })
-//     .on("error", err => cb(err));
-// };
+
 module.exports = {
   myRequest
-  // uncomment line below to export bonus solution
-  // ,myBonusRequest
+
 };
