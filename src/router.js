a227c6972c11bbf4c095bc00c47490e59865f892
renatajarmova <renatajarmova@gmail.com>
2019-11-28 16:09:31 +0000

    Add empty city variable to be updated later
    Co-Authored-by: <redahaq@gmail.com>

--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ const handlers = require("./handlers.js");
 const config = require("./config.json");
 const apis = require("./api");
 
+// let city = 
 const tflKey = config.TFL_KEY;
 const tflAppID = config.TFL_APP_ID;
 const transportUrl = `https://transportapi.com/v3/uk/public/journey/from/london/to/liverpool.json?app_id=${tflAppID}&app_key=${tflKey}`;
-let city = "London";
+
 const weatherKey = config.WEATHER_KEY; // fetch key from config file
-const weatherUrl = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${weatherKey}`;
+const weatherUrl = `https://api.openweathermap.org/data/2.5/find?units=metric&appid=${weatherKey}&q=${city}`;
 
 const router = (request, response) => {
   const endpoint = request.url;
