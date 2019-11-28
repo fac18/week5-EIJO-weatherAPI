5e0f9db82ca9385e8835fc134ae5cd9865caf152
renatajarmova <renatajarmova@gmail.com>
2019-11-28 15:37:36 +0000

    Add first atempt to weather api
    Co-authored-by: <redahaq@gmail.com>

--- src/api.js
+++ src/api.js
@@ -3,13 +3,25 @@ const http = require("http");
 const config = require("./config");
 const https = require("https");
 
-const weatherKey = config.WEATHER_KEY; // fetch key from config file
-const url = `http://api.openweathermap.org/data/2.5/weather?&units=metric?appid=${weatherKey}&q=`;
+const weatherRequest = (url, cb) => {
+  const protocol = url.includes("https") ? https : http;
+  protocol
+    .get(url, response => {
+      let data = "";
+      response.on("data", chunk => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        const body = JSON.parse(data);
+        console.log(body.weather.description);
 
-
-// const tflKey = config.TFL_KEY;
-// const tflAppID = config.TFL_APP_ID;
-// // const transportUrl = `https://transportapi.com/v3/uk/public/journey/from/london/to/liverpool.json?app_id=${tflAppID}&app_key=${tflKey}`;
+        const statusCode = response.statusCode;
+        console.log("StatusCode:", statusCode);
+        // cb(null, { statusCode, body });
+      });
+    })
+    .on("error", err => cb(err));
+};
 
 const transportRequest = (url, cb) => {
   const protocol = url.includes("https") ? https : http;
