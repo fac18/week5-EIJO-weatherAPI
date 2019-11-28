7fe1132eb7f0b2e0f138909e5abe1fa87fbeb04e
renatajarmova <renatajarmova@gmail.com>
2019-11-28 16:02:45 +0000

    Access weather description and icon with the API call
    Co-Authored-by: <redahaq@gmail.com>

--- src/api.js
+++ src/api.js
@@ -13,7 +13,10 @@ const weatherRequest = (url, cb) => {
       });
       response.on("end", () => {
         const body = JSON.parse(data);
-        console.log(body.weather.description);
+        // console.log('Whole body weather: ', body)
+        let weatherCondition = body.list[0].weather[0].description;
+        let weatherIconCode = body.list[0].weather[0].icon;
+        let weatherIcon = `http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`
 
         const statusCode = response.statusCode;
         console.log("StatusCode:", statusCode);
@@ -64,7 +67,8 @@ const transportRequest = (url, cb) => {
 module.exports = {
   // myRequest,
   // url,
-  transportRequest
+  transportRequest,
+  weatherRequest
   // uncomment line below to export bonus solution
   // ,myBonusRequest
 };
--- src/router.js
+++ src/router.js
@@ -5,10 +5,9 @@ const apis = require("./api");
 const tflKey = config.TFL_KEY;
 const tflAppID = config.TFL_APP_ID;
 const transportUrl = `https://transportapi.com/v3/uk/public/journey/from/london/to/liverpool.json?app_id=${tflAppID}&app_key=${tflKey}`;
-
+let city = "London";
 const weatherKey = config.WEATHER_KEY; // fetch key from config file
 const weatherUrl = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=${weatherKey}`;
-let city = "London";
 
 const router = (request, response) => {
   const endpoint = request.url;
